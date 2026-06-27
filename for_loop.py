dc075581d3cbcf63f082834a21175018598e503b
Jaq <131463413+jaqsouza@users.noreply.github.com>
2026-06-27 19:14:23 -0300

    Change loop to print a different message six times

--- for_loop.py
+++ for_loop.py
@@ -1,6 +1,7 @@
-for i in range (2):
-    print('hello')
+for i in range (6):
+    print('acredito no hexa')
 
-# Este código imprime na tela a palavra "hello" duas vezes.
+# Este código imprime na tela a frase "acredito no hexa" 6 vezes.
 # "i" é um valor que aumenta cada vez que ele é usado.
-# Lembre-se que na computação tudo começa a partir do 0 e não do 1, então 2 valores seguindos são 0 e 1, não 0 e 2.
+# Lembre-se que na computação tudo começa a partir do 0 e não do 1, então 2 valores seguindos são 0 e 1, não 1 e 2.
+# Logo, 6 vezes é 0, 1, 2, 3, 4, 5.
