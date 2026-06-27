168b2d551fc1f9f021772e07fece0e522882fe13
Jaq <131463413+jaqsouza@users.noreply.github.com>
2026-06-27 19:17:41 -0300

    Update for_loop.py

--- for_loop.py
+++ for_loop.py
@@ -4,4 +4,7 @@ for i in range (6):
 # Este código imprime na tela a frase "acredito no hexa" 6 vezes.
 # "i" é um valor que aumenta cada vez que ele é usado.
 # Lembre-se que na computação tudo começa a partir do 0 e não do 1, então 2 valores seguindos são 0 e 1, não 1 e 2.
-# Logo, 6 vezes é 0, 1, 2, 3, 4, 5.
+# Logo, 6 vezes é 0, 1, 2, 3, 4, 5. Rodando o seguinte exeplo é possível entender melhor isso:
+
+for i in range (6):
+    print(i)
