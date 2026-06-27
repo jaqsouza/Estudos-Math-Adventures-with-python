37c10fcc7939c7063cc7cb4e2c7c70b2b2a0332a
Jaq <131463413+jaqsouza@users.noreply.github.com>
2026-06-27 18:56:01 -0300

    Create myturtle.py for turtle graphics demonstration
    Added a basic turtle graphics example with comments.

---
+++ myturtle.py
@@ -0,0 +1,14 @@
+# Este é o primeiro código que o livro nos ensina a fazer.
+# Descobri que "turtle" é uma biblioteca real do python que já vem com ele.
+# Com a biblioteca "turtle" é possível fazer imagens gráficas.
+
+from turtle import *
+forward(100)
+shape('turtle')
+right(45)
+forward(150)
+
+# Em 'import *" o "*" quer dizer "traga tudo dessa biblioteca".
+# "forward" é para quantos passos, ou melhor, pixeis, o que vemos na tela irá andar.
+# "shape" é o formato que damos ao que aparece na tela, este comando sem o "shape" trás uma seta, mas com o "turtle" junto do comando ele muda a seta para uma tartaruga.
+# "right" é para mudar a direção da tartaruga, no caso 45 graus.
