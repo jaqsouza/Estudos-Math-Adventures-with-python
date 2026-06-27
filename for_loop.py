ee24d41c8c03eaf0d74a0c4cdfaecf8b12a6b6be
Jaq <131463413+jaqsouza@users.noreply.github.com>
2026-06-27 19:00:34 -0300

    Create for_loop.py

---
+++ for_loop.py
@@ -0,0 +1,6 @@
+for i in range (2):
+    print('hello')
+
+# Este código imprime na tela a palavra "hello" duas vezes.
+# "i" é um valor que aumenta cada vez que ele é usado.
+# Lembre-se que na computação tudo começa a partir do 0 e não do 1, então 2 valores seguindos são 0 e 1, não 0 e 2.
